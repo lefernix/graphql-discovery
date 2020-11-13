e03f99da7214215374e02ff0f21130e49149dfa6
lefernix <adrien.nombalier@fasst.io>
2020-11-13 12:14:02 +0100

    challenge done with nasa API

---
+++ src/components/Launche.js
@@ -0,0 +1,40 @@
+import { useQuery, gql } from "@apollo/client";
+
+const LAUNCHES = gql`
+  {
+    launches(limit: 5) {
+      launch_date_utc
+      launch_success
+      rocket {
+        rocket_name
+      }
+      links {
+        video_link
+      }
+      details
+    }
+  }
+`;
+
+const LaunchesNasa = () => {
+  const { loading, error, data } = useQuery(LAUNCHES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.launches.map((launche) => (
+    <>
+      <div key={launche.rocket_name}>
+        <ul>
+          <li>Date UTC : {launche.launch_date_utc}</li>
+          <li>Resultat : {launche.launch_success ? "succès" : "raté"}</li>
+          <li>Nom : {launche.rocket.rocket_name}</li>
+          <li>Vidéo : {launche.links.video_link}</li>
+          <li>Détails : {launche.details}</li>
+        </ul>
+      </div>
+    </>
+  ));
+};
+
+export default LaunchesNasa;
